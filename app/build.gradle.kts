e2c3da5f24c4f53b97746a935f768a509ead1677
HirooHG <hugo.golliet@etu.univ-smb.fr>
2023-12-22 11:38:20 +0100

    feat: added icon and app name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
